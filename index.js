b94b3e1b1c07796ca69806f9234ff2e3febb91d9
SS Jones <SSJones.code@gmail.com>
2020-08-25 16:56:51 -0700

    I added the .then function as well as starting the fs.writeFile method. I ran npm init and I ran npm install inquirer so I have a package.json

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // * The generated README includes 1 badge that's specific to the repository.
+
+//pull in inquirer and fs to write to the file
+const inquirer = require("inquirer");
+const fs = require("fs");
 // array of questions for user
 const questions = [];
 
+//reference the var on line 1 and use the prompt method()
+//this is asyncronous
 inquirer.prompt([
   {
     type: "input",
@@ -48,10 +54,27 @@ inquirer.prompt([
     message: "Please provide the questions for the work in question.",
     name: "questionsInput",
   },
-]);
-//pull in inquirer and fs to write to the file
-const inquirer = require("inquirer");
-const fs = require("fs");
+])
+//below you see what happens WHEN the response returns
+.then(function (response) {
+    //strigify the returned response WITH FORMATTING
+    let obj = JSON.stringify(response, null, 2);
+    console.log(obj);
+
+    //need to add this to the .then statement, correct?
+    fs.appendFile("./users.txt", obj + "\n", function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("complete");
+    });
+  });
+
+
+    //strigify the returned response WITH FORMATTING:
+    // let obj = JSON.stringify(response, null, 2);
+    // console.log(obj);
+
 
 // function to write README file
 function writeToFile(fileName, data) {}
