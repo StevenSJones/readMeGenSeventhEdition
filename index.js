778ffcc2edd84eaf37021ab2b135ecdb668f61a9
SS Jones <SSJones.code@gmail.com>
2020-08-25 16:31:29 -0700

    I fixed the readme so that i could actually read it. I also added the requiring of the inquirer method  as well as the fs.writeFile method and all of their dependencies.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+// * The generated README includes the following sections: 
+
+//   * Title
+//   * Description
+//   * Table of Contents
+//   * Installation
+//   * Usage
+//   * License
+//   * Contributing
+//   * Tests
+//   * Questions
+
+// * The generated README includes 1 badge that's specific to the repository.
 // array of questions for user
 const questions = [
 
 ];
+
+
+
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // function to write README file
 function writeToFile(fileName, data) {
