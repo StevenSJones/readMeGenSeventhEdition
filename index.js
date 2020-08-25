ba68349e0296a9df90920602c9ae9b6a1e7f96ea
SS Jones <SSJones.code@gmail.com>
2020-08-25 16:45:48 -0700

    I added the questions in the form of a .prompt like we did in class. Added some comments as well.

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
-// * The generated README includes the following sections: 
-
-//   * Title
-//   * Description
-//   * Table of Contents
-//   * Installation
-//   * Usage
-//   * License
-//   * Contributing
-//   * Tests
-//   * Questions
-
 // * The generated README includes 1 badge that's specific to the repository.
 // array of questions for user
-const questions = [
+const questions = [];
 
-];
-
-
-
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is your title for the work in question?",
+    name: "titleInput",
+  },
+  {
+    type: "input",
+    message: "Please provide a description of the work in question.",
+    name: "descriptionInput",
+  },
+  {
+    type: "input",
+    message: "Please provide a table of contents for the work in question.",
+    name: "tableOfContentsInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the installation for the work in question.",
+    name: "installationInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the usage for the work in question.",
+    name: "usageInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the license for the work in question.",
+    name: "licenseInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the contributing for the work in question.",
+    name: "contributingInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the tests for the work in question.",
+    name: "testsInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the questions for the work in question.",
+    name: "questionsInput",
+  },
+]);
+//pull in inquirer and fs to write to the file
 const inquirer = require("inquirer");
 const fs = require("fs");
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
