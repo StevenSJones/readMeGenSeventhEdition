6df0979f4d748d13d5aa4682d30b9def9a33e577
SS Jones <SSJones.code@gmail.com>
2020-08-27 09:44:53 -0700

    I made the code modular by removing the   //responseObj = generateReadMe(responseObj); section and instead passing the response into the function genrateMarkdoan so that I could slim down the index.js file itself.

--- index.js
+++ index.js
@@ -1,80 +1,84 @@
-// * The generated README includes 1 badge that's specific to the repository.
-
 //pull in inquirer and fs to write to the file
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your GITHUB username?",
+    name: "githubUser",
+  },
+  {
+    type: "input",
+    message: "What is your GITHUB repo name?",
+    name: "githubRepo",
+  },
+  {
+    type: "input",
+    message: "What is your title for the work in question?",
+    name: "titleInput",
+  },
+  {
+    type: "input",
+    message: "Please provide a description of the work in question.",
+    name: "descriptionInput",
+  },
+  {
+    type: "input",
+    message: "Please provide a table of contents for the work in question.",
+    name: "tableOfContentsInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the installation for the work in question.",
+    name: "installationInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the usage for the work in question.",
+    name: "usageInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the license for the work in question.",
+    name: "licenseInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the contributing for the work in question.",
+    name: "contributingInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the tests for the work in question.",
+    name: "testsInput",
+  },
+  {
+    type: "input",
+    message: "Please provide the questions for the work in question.",
+    name: "questionsInput",
+  },
+];
 
-// function to initialize program
-// function init() {}
 //reference the var on line 1 and use the prompt method()
 //this is asyncronous
 inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your title for the work in question?",
-      name: "titleInput",
-    },
-    {
-      type: "input",
-      message: "Please provide a description of the work in question.",
-      name: "descriptionInput",
-    },
-    {
-      type: "input",
-      message: "Please provide a table of contents for the work in question.",
-      name: "tableOfContentsInput",
-    },
-    {
-      type: "input",
-      message: "Please provide the installation for the work in question.",
-      name: "installationInput",
-    },
-    {
-      type: "input",
-      message: "Please provide the usage for the work in question.",
-      name: "usageInput",
-    },
-    {
-      type: "input",
-      message: "Please provide the license for the work in question.",
-      name: "licenseInput",
-    },
-    {
-      type: "input",
-      message: "Please provide the contributing for the work in question.",
-      name: "contributingInput",
-    },
-    {
-      type: "input",
-      message: "Please provide the tests for the work in question.",
-      name: "testsInput",
-    },
-    {
-      type: "input",
-      message: "Please provide the questions for the work in question.",
-      name: "questionsInput",
-    },
-  ])
+  .prompt(questions)
   //below you see what happens WHEN the response returns
   .then(function (response) {
+    console.log("response: ", response);
     //strigify the returned response WITH FORMATTING
-    let responseObj = JSON.stringify(response, null, 4);
+    //let responseObj = JSON.stringify(response, null, "\t");
+    let responseObj = response;
     console.log(responseObj);
-    // function to write README file
-    // function writeToFile(fileName, data) {}
+    //instead of stringify I am passing the response obj into the function and since the 
+   responseObj = generateMarkdown(responseObj);
+
     fs.writeFile("readME.md", responseObj, function (err) {
       if (err) {
         return console.log(err);
       }
-
       console.log("Success!");
     });
   });
-
-
-// // function call to initialize program
-// init();
-
